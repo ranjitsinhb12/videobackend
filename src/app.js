5813bae4dee57a4d561534fa323df4d0023a1b8a
ranjitsinhb12 <ranjitsinhb12@gmail.com>
2023-12-08 23:12:43 +1030

    Utils and User and Video models

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+// will receive jason data setting 
+app.use(express.json({ limit: "12kb" }))
+// url data receive setting
+app.use(express.urlencoded({ extended: true, limit: "12kb" }))
+// file folder store setting  This is a public folder
+app.use(express.static("public"))
+// Access and accept cookies 
+app.use(cookieParser())
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import express from "express"
 
 
 dotenv.config({
     path: "./env"
 })
 
-connectDB();
+connectDB()
+
+    .then(() => {
+        app.on("error", (err) => {
+            console.log("Error: ", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port : ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("Database connection Faild!!", err)
+    })
